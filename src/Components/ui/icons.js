513780087bde7dd5c9d67c138072198eb6641e6b
frozzi18 <42233054+frozzi18@users.noreply.github.com>
2019-04-25 12:49:05 +0200

    Updata components

--- src/Components/ui/icons.js
+++ src/Components/ui/icons.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
-import mcitylogo from '../../Resources/images/logos/manchester_city_logos.png';
+import mcitylogo from '../../Resources/images/logos/manchester_city_logo.png';
 
 export const CityLogo = (props) => {
 
     const template = 
-    <div>
-        logo    
-    </div>
+    <div
+        className="img_cover"
+        style={{
+            width:props.width, 
+            height: props.height,
+            background:`url(${mcitylogo}) no-repeat`
+        }}
+    ></div>
 
     if(props.link){
         return (
