5de03160824ca7624f402abf0f9eb4a42cc1142e
frozzi18 <42233054+frozzi18@users.noreply.github.com>
2019-04-25 12:02:08 +0200

    Create icons.js

---
+++ src/Components/ui/icons.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+import mcitylogo from '../../Resources/images/logos/manchester_city_logos.png';
+
+export const CityLogo = (props) => {
+
+    const template = 
+    <div>
+        logo    
+    </div>
+
+    if(props.link){
+        return (
+            <Link to={props.linkTo} className="link_logo">
+                {template}
+            </Link>
+        )
+    } else {
+        return template
+    }
+}
