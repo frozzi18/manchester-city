6592fff2f9cf777d160ca0d915a58237f3239037
frozzi18 <42233054+frozzi18@users.noreply.github.com>
2019-04-26 07:13:33 +0200

    Update Components

---
+++ src/Components/home/featured/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Featured = () => {
+    return (
+        <div className="featured_wrapper">
+            Some Text
+        </div>
+    );
+};
+
+export default Featured;
